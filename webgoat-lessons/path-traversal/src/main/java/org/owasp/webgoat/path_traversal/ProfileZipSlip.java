65aacc86289a85159f8760230eed0f87b45e4360
Philipp Fruck <dev@p-fruck.de>
2021-11-25 16:56:08 +0100

    Fix zip slip (sick!)
    Co-authored-by: Pius Walter <pius.walter@gmx.net>

--- webgoat-lessons/path-traversal/src/main/java/org/owasp/webgoat/path_traversal/ProfileZipSlip.java
+++ webgoat-lessons/path-traversal/src/main/java/org/owasp/webgoat/path_traversal/ProfileZipSlip.java
@@ -52,6 +52,9 @@ public class ProfileZipSlip extends ProfileUploadBase {
                 while (entries.hasMoreElements()) {
                     ZipEntry e = entries.nextElement();
                     File f = new File(tmpZipDirectory.toFile(), e.getName());
+                    if (!file.toPath().normalize().startsWith(destinationDir.toPath())) {
+                        throw new Exception("Bad zip entry");
+                    }
                     InputStream is = zip.getInputStream(e);
                     Files.copy(is, f.toPath(), StandardCopyOption.REPLACE_EXISTING);
                 }
