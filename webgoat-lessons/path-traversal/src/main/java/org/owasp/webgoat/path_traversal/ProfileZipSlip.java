d952dc04508cd3a951dd85a1619a8adf05116b7b
Philipp Fruck <dev@p-fruck.de>
2021-11-25 17:02:57 +0100

    Fixed fix
    Co-fixed-by: Pius Walter <pius.walter@gmx.net>

--- webgoat-lessons/path-traversal/src/main/java/org/owasp/webgoat/path_traversal/ProfileZipSlip.java
+++ webgoat-lessons/path-traversal/src/main/java/org/owasp/webgoat/path_traversal/ProfileZipSlip.java
@@ -52,7 +52,7 @@ public class ProfileZipSlip extends ProfileUploadBase {
                 while (entries.hasMoreElements()) {
                     ZipEntry e = entries.nextElement();
                     File f = new File(tmpZipDirectory.toFile(), e.getName());
-                    if (!file.toPath().normalize().startsWith(destinationDir.toPath())) {
+                    if (!file.toPath().normalize().startsWith(tmpZipDirectory.toFile().toPath())) {
                         throw new Exception("Bad zip entry");
                     }
                     InputStream is = zip.getInputStream(e);
